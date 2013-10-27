5012124038f3b48d528f97a4fe3ea48e83495672
Bigy95 <bigybard@gmail.com>
2013-10-27 21:18:56 +0100

    Update PlaceLaser.ml
    il manque les fonctions pour les rayons

--- Code/PlaceLaser.ml
+++ Code/PlaceLaser.ml
@@ -3,9 +3,9 @@ Let lanceray = fun (x,y) -> match (w,y) with
 	|(0,n) -> print_string("Rayon invalide !")
 	|(m,0) -> print_string("Rayon invalide !")
 	|(m,n) -> print_string("Rayon invalide !")
-	|(_,0) -> rayon vers le bas
-	|(0,_) -> rayon vers la droite
+	|(_,0) -> rayon vers le bas 
+	|(0,_) -> rayon vers la droite 
 	|(_,n) -> rayon vers le haut
-	|(m,_) -> rayon vers le haut
+	|(m,_) -> rayon vers le gauche
 	|(_,_) -> print_string("Rayon invalide !") ;;
 	
